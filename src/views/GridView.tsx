36165f38a09e470556d7cd426f0f5169865199be
bima1313 <bimairfy13@gmail.com>
2026-02-02 04:33:10 +0700

    refactor: simplify Card component props by removing param totalData

--- src/views/GridView.tsx
+++ src/views/GridView.tsx
@@ -1,24 +1,15 @@
 import type { FC } from "react";
 import Card from "../components/Card";
-import { periodicData } from "../data/periodicData";
 import type { UsersData } from "../models/dataUsers";
 
-interface props{
-    usersData?: UsersData;
+interface props {
+  usersData?: UsersData;
 }
-const GridView: FC<props> = ({usersData}) => {
+const GridView: FC<props> = ({ usersData }) => {
   return (
     <group>
       {usersData?.data.map((user, index) => {
-        return (
-          <Card
-            key={index}
-            userData={user}
-            index={index}
-            mode="Grid"
-            totalData={periodicData.length}
-          />
-        );
+        return <Card key={index} userData={user} index={index} mode="Grid" />;
       })}
     </group>
   );
