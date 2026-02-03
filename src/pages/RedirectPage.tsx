9149d6a11ef5f1f874cf2d0a9ec076fa5dc59a4a
bima1313 <bimairfy13@gmail.com>
2026-02-03 21:56:31 +0700

    refactor: use InformationCard in RedirectPage

--- src/pages/RedirectPage.tsx
+++ src/pages/RedirectPage.tsx
@@ -1,23 +1,23 @@
 import { useNavigate } from "react-router-dom";
+import InformationCard from "../components/InformationCard";
 import { Path } from "../routes/routePath";
 
 export default function RedirectPage() {
   const navigate = useNavigate();
   return (
-    <div className="w-full h-screen flex justify-center items-center bg-black select-none">
-      <div className="border-2 bg-white rounded-xl px-24 py-16 text-center border-detail custom-box-shadow on-hover">
-        <h1 className="text-2xl font-bold mb-5">Requried Login</h1>
-        <h2>You must be logged in to view to page</h2>
-        <button
-          className="mt-8 w-full rounded-full px-4 py-[4px] bg-blue-600 text-xl font-bold text-white cursor-pointer"
-          onClick={() => {
-            navigate(Path.initPath);
-          }}
-        >
-          OK
-        </button>
-        <h3 className="mt-2 text-sm text-slate-600">Redirect to home page</h3>
-      </div>
-    </div>
+    <InformationCard
+      title="Requried Login"
+      description="You must be logged in to view to page"
+      footer="Redirect to home page"
+    >
+      <button
+        className="mt-8 w-full rounded-full px-4 py-[4px] bg-blue-600 text-xl font-bold text-white cursor-pointer"
+        onClick={() => {
+          navigate(Path.initPath);
+        }}
+      >
+        OK
+      </button>
+    </InformationCard>
   );
 }
